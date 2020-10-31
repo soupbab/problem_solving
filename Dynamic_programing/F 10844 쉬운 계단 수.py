40d259f7ee36ca6bbbd674fdd894b01787c0f12c
soupbab <jungu1106@naver.com>
2020-11-01 02:31:04 +0900

    2020.11.01 ps

---
+++ Dynamic_programing/F 10844 쉬운 계단 수.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+dp = [[0] * 10 for i in range(n)]
+
+for i in range(1, 10):
+    dp[0][i] = 1
+for i in range(1, n):
+    for j in range(10):
+        if j > 0:
+            dp[i][j] += dp[i - 1][j - 1]
+
+        if j < 9:
+            dp[i][j] += dp[i - 1][j + 1]
+
+        dp[i][j] %= 1000000000
+
+print(sum(dp[n - 1]) % 1000000000)
