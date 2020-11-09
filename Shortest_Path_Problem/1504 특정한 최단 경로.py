1b6ef3da6c72cbb2584bc4c2bce4c668ae67d0d9
soupbab <jungu1106@naver.com>
2020-11-09 23:11:43 +0900

    2020.11.09 ps

--- Shortest_Path_Problem/1504 특정한 최단 경로.py
+++ Shortest_Path_Problem/1504 특정한 최단 경로.py
@@ -1,23 +1,49 @@
+# import sys
+#
+# n, e = map(int, input().split())
+#
+# INF = int(1e9)
+# table = [[INF] * (n + 1) for _ in range(n + 1)]
+#
+# for i in range(1, n + 1):
+#     for j in range(1, n + 1):
+#         if i == j:
+#             table[i][j] = 0
+#
+# for _ in range(e):
+#     a, b, c = map(int, sys.stdin.readline().rstrip().split())
+#     table[a][b] = c
+#     table[b][a] = c
+#
+# for k in range(n + 1):
+#     for i in range(n + 1):
+#         for j in range(n + 1):
+#             table[i][j] = min(table[i][j], table[i][k] + table[k][j])
+#
+# p1, p2 = map(int, input().split())
+# distance = min(table[1][p1] + table[p1][p2] + table[p2][n], table[1][p2] + table[p2][p1] + table[p1][n])
+#
+# print(distance) if distance < INF else print(-1)
+
+########################################################################################################################
+
 import sys
 import heapq
 
 n, e = map(int, input().split())
-
 graph = [[] for _ in range(n + 1)]
 for _ in range(e):
     a, b, c = map(int, sys.stdin.readline().rstrip().split())
     graph[a].append((b, c))
-
-p1, p2 = map(int, input().split())
-check = [[False] * 2 for _ in range(n + 1)]
+    graph[b].append((a, c))
 
 INF = int(1e9)
-distance = [INF] * (n + 1)
 
 
 def dijkstra(start):
     q = []
     heapq.heappush(q, (0, start))
+    distance = [INF] * (n + 1)
     distance[start] = 0
 
     while q:
@@ -30,8 +56,17 @@ def dijkstra(start):
             cost = dist + i[1]
 
             if cost < distance[i[0]]:
-                heapq.heappush(q, (cost, i[0]))
                 distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-                if i[]
+    return distance
 
+
+p1, p2 = map(int, input().split())
+
+start_ = dijkstra(1)
+p1_ = dijkstra(p1)
+p2_ = dijkstra(p2)
+
+answer = min(start_[p1] + p1_[p2] + p2_[n], start_[p2] + p2_[p1] + p1_[n])
+print(answer if answer < INF else -1)
