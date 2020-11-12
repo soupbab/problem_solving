ebe73706db40b3ecb1f96bd8898bbf1e1ed4d9c9
soupbab <jungu1106@naver.com>
2020-11-12 20:51:51 +0900

    2020.11.12 ps

--- Shortest_Path_Problem/pypy 1956 운동.py
+++ Shortest_Path_Problem/pypy 1956 운동.py
@@ -4,10 +4,6 @@ v, e = map(int, input().split())
 
 INF = int(1e9)
 table = [[INF] * (v + 1) for _ in range(v + 1)]
-for i in range(v + 1):
-    for j in range(v + 1):
-        if i == j:
-            table[i][j] = 0
 
 for _ in range(e):
     a, b, c = map(int, sys.stdin.readline().rstrip().split())
@@ -18,10 +14,8 @@ for k in range(v + 1):
         for j in range(v + 1):
             table[i][j] = min(table[i][j], table[i][k] + table[k][j])
 
-plans = []
-for i in range(v + 1):
-    for j in range(v + 1):
-        if i != j:
-            plans.append(table[i][j] + table[j][i])
+answer = INF
+for i in range(1, v + 1):
+    answer = min(answer, table[i][i])
 
-print(min(plans) if min(plans) < INF else -1)
+print(answer if answer < INF else -1)
