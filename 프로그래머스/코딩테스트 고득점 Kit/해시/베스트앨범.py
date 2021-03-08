41f585ff2cd1eaed302fc92671045ca6e9665ff3
ChoiJunGu <67000572+soupbab@users.noreply.github.com>
2021-03-08 21:17:52 +0900

    Create 베스트앨범.py

---
+++ 프로그래머스/코딩테스트 고득점 Kit/해시/베스트앨범.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+def solution(genres, plays):
+    play_count = defaultdict(int)
+    for genre, play in zip(genres, plays):
+        play_count[genre] += play
+
+    zipped_list = [(genre, play, index) for index, (genre, play) in enumerate(zip(genres, plays))]
+    zipped_list.sort(key=lambda x: x[2])
+    zipped_list.sort(key=lambda x: x[1], reverse=True)
+    zipped_list.sort(key=lambda x: play_count[x[0]], reverse=True)
+    
+    answer = []
+    genre_count = defaultdict(int)
+    for item in zipped_list:
+        if genre_count[item[0]] < 2:
+            answer.append(item[2])
+            genre_count[item[0]] += 1
+    return answer
+  
