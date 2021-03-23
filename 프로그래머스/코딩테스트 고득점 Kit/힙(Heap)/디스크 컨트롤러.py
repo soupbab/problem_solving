8c136f38339d00c049bc55f8595dbe1ba3c0f781
ChoiJunGu <67000572+soupbab@users.noreply.github.com>
2021-03-23 23:23:22 +0900

    Create 디스크 컨트롤러.py

---
+++ 프로그래머스/코딩테스트 고득점 Kit/힙(Heap)/디스크 컨트롤러.py
@@ -0,0 +1,32 @@
+# 
+
+import heapq
+
+def solution(jobs):
+    length = len(jobs)
+    answer = 0
+    time = 0
+    
+    jobs.sort(key=lambda x: x[1])
+    jobs.sort(key=lambda x: x[0])
+    
+    heap = []
+    while jobs or heap:
+        jobs_ = jobs.copy()
+        for job in jobs_:
+            request_time, necessary_time = job
+            if request_time <= time:
+                jobs.remove(job)
+                heapq.heappush(heap, [necessary_time, request_time])
+                
+        if not heap:
+            fastest = jobs.pop(0)
+            heapq.heappush(heap, reversed(fastest))
+            
+        task = list(heapq.heappop(heap))
+        time = max(time, task[1])
+        time += task[0]
+        answer += time - task[1]
+    
+    answer /= length
+    return answer
